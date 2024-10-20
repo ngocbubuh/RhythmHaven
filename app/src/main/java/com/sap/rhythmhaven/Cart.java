196b1567aa0a92724bfa80e2117ac1de8b940e6e
tiendungdayyy <91593609+tiendungdayyy@users.noreply.github.com>
2024-10-20 18:42:09 +0700

    fixing CI

--- app/src/main/java/com/sap/rhythmhaven/Cart.java
+++ app/src/main/java/com/sap/rhythmhaven/Cart.java
@@ -1,21 +1,22 @@
 package com.sap.rhythmhaven;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Cart extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cart);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        // Apply window insets to the root layout
+        ConstraintLayout cartLayout = findViewById(R.id.cart_layout);
+        ViewCompat.setOnApplyWindowInsetsListener(cartLayout, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
